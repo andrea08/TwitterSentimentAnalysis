42e37722947e61857884fea1627dd4544ec24d09
andrea08 <andrea1391@gmail.com>
2018-02-02 23:38:24 +0100

    Structure of settings, each developer will have his/her own settings to adapt to each enviroment. Every developer`s seetings will import from base where the general settings are described. Each developer will need to specify the details of the database, in order to run the porject.

--- restfulapi/restfulapi/settings/base.py
+++ restfulapi/restfulapi/settings/base.py
@@ -22,9 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '_-t7vgw8t@1+nw@6nh1j3b*h%43%=hvomb=$(7g%t6!apf-+0&'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -48,6 +45,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CSRF_COOKIE_HTTPONLY = True
 
 ROOT_URLCONF = 'restfulapi.urls'
 
@@ -75,8 +74,11 @@ WSGI_APPLICATION = 'restfulapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_name',
+        'USER': 'user_name',
+        'PASSWORD': 'password',
+        'HOST': 'db_host',
     }
 }
 
