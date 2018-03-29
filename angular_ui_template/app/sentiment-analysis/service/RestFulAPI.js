b36fb4573e0f17c1ca209cda4ae668e88dc58caf
andrea08 <andrea1391@gmail.com>
2018-03-29 21:25:51 +0200

    First test to communicate angular and django applications. Get method coudn't be used for sending json object data withing the request, therefoere in the restfulAPI the view was changed to also allow post request so the data can be sent.

--- angular_ui_template/app/sentiment-analysis/service/RestFulAPI.js
+++ angular_ui_template/app/sentiment-analysis/service/RestFulAPI.js
@@ -5,12 +5,44 @@ angular.module('app.sentimentAnalysis').provider('RestFulAPI', function () {
 	* This provider will send the request to the Django RestFulAPI, and process the responses as well.
 	*/
 	// dev
-	let server= 'http://127.0.0.1:8000/tweet_sentiment/sentiment'
+	let server= 'http://127.0.0.1:8000/';
+
+	var data = {
+        "tweets": [
+            {
+                "author": "gossminn",
+                "date_time": '1522351047.353921',
+                "content": "Hello world! I love organic cookies!"
+            },
+            {
+                "author": "gossminn",
+                "date_time": '1522351047.353921',
+                "content": "I hate studying!"
+            }
+        ]
+    };
 
 
-	this.$get = function(){
+	this.$get = function($q, $http){
+		var get_analysis = function(data){
+			var url = server + 'tweet_sentiment/sentiment';
+	    	var deffered = $q.defer();
+	    	$http.post(url, data).success(function(res){
+		 		console.log('Good');				
+				console.log(res)
+				deffered.resolve('Hey');
+			}).error(function(err){
+				console.error('Bad')
+				console.error(JSON.stringify(err));
+				console.log(err);
+				deffered.reject(err);
+			});
+			return deffered.promise;
+	    };
 		return {
+			
 			list: function(){
+				get_analysis(data);
 				return ['a', 'b','c'];
 			}
 		}
