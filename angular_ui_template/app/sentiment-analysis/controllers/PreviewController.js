e9834e04ce26a6ac52662d44848a5fada1755757
andrea08 <andrea1391@gmail.com>
2018-03-30 03:11:25 +0200

    Provider to communicate with Django restFulAPI is already finished. View of preview has now the struture for present the different responses of the request. Also controller of the preview was updated.

--- angular_ui_template/app/sentiment-analysis/controllers/PreviewController.js
+++ angular_ui_template/app/sentiment-analysis/controllers/PreviewController.js
@@ -1,17 +1,43 @@
 'use strict';
 
 angular.module('app.sentimentAnalysis').controller('PreviewController', function ($scope, $stateParams, RestFulAPI, $state) {
-	$scope.query = $stateParams.query;
+	// Attributes of the controller
 
-	
+	$scope.query = $stateParams.query;
+	$scope.twees = false;
+	$scope.total_tweets = null;
+	$scope.tweet_list = null;
+	$scope.error_message = null;
 
 	// Redirect if there is no query value
 	if($scope.query == null){
 		$state.go('app.home');
 	}else{
 		// There is a query value
-		
-		$scope.list = RestFulAPI.list();
+		var tweet_query = {type: null, content: $scope.query};
+		if($scope.query[0] ==  '@'){
+			tweet_query.type = "nickname"
+		}else{
+			tweet_query.type = "hashtag"
+		}
+
+		RestFulAPI.get_tweets(tweet_query).then(function(tweet_list){
+			$scope.tweet_list = tweet_list.tweets;
+			$scope.tweets = true;
+			$scope.total_tweets = len(tweet_list.tweets);
+
+
+		}, function(err){
+			$scope.twees = false;
+			$scope.total_tweets = null;
+			$scope.tweet_list = null;
+
+			if(err.type == 0){
+				$scope.error_message = "Tweets not found :(";
+			}else{
+				$scope.error_message = "Something went wrong :(";
+			}
+		});
 	}
 
 	
--- angular_ui_template/app/sentiment-analysis/service/RestFulAPI.js
+++ angular_ui_template/app/sentiment-analysis/service/RestFulAPI.js
@@ -7,7 +7,10 @@ angular.module('app.sentimentAnalysis').provider('RestFulAPI', function () {
 	// dev
 	let server= 'http://127.0.0.1:8000/';
 
-	var data = {
+	// Start of data >>>>>>>>>>>>>>>>>>
+	// Dummi data. For development
+
+	var sentiment_query_dev = {
         "tweets": [
             {
                 "author": "gossminn",
@@ -22,28 +25,83 @@ angular.module('app.sentimentAnalysis').provider('RestFulAPI', function () {
         ]
     };
 
+    var tweet_query_dev = {
+    	"type": "hashtag",
+    	"content": "#Dancing"
+    };
+
+    // End of data >>>>>>>>>>>>>>>>>>>>
+
 
 	this.$get = function($q, $http){
-		var get_analysis = function(data){
+		/**
+		* Request to retrieve the sentiment analysis of a set of tweets.
+		* sentiment_query = {tweets: [{author: "nickname", data_time:"time", content:"tweet"} , <tweet> ..]}
+		* sentiment_data = {number_found: int, skipped: int, score: {positive_number: <int>, 
+		* positive_percentage: <float>, negative_number: <int>, negative_percentage: <float>, 
+		* neutral_number: <int>, neutral_percentage: <float>}}
+		* @param sentiment_query
+		* return a promise with the response of the request. This can be a sentiment_data if the response is success, or
+		* an error either in the client side or server side.
+		*/
+		var get_analysis = function(sentiment_query){
 			var url = server + 'tweet_sentiment/sentiment';
-	    	var deffered = $q.defer();
-	    	$http.post(url, data).success(function(res){
-		 		console.log('Good');				
-				console.log(res)
-				deffered.resolve('Hey');
+	    	var dfd = $q.defer();
+	    	$http.post(url, sentiment_query).success(function(sentiment_data){
+				dfd.resolve(sentiment_data);
 			}).error(function(err){
-				console.error('Bad')
+				var error = {type: 1, message: JSON.stringify(err), data: err};
 				console.error(JSON.stringify(err));
-				console.log(err);
-				deffered.reject(err);
+				dfd.reject(error);
 			});
-			return deffered.promise;
+			return dfd.promise;
 	    };
+
+	    /**
+	    * Request the tweets based on a tweet_query.
+	    * tweet_query = {type: "hashtag"|"nickname", content:"<query>"}
+	    * @param tweet_query
+	    * return a promise with the response of the request. This can be a list of the tweets retrieve,
+	    * an empty list if there were no tweets, or an error either in the client side or server side.
+	    */
+	    var get_tweets = function(tweet_query){
+	    	/**
+	    	* Type of errors:
+	    	* type 0 when there were no tweets found
+	    	* type 1 when the response has an error status code
+	    	*/
+
+	    	var url = 'tweet_sentiment/tweets';
+	    	var dfd = $q.defer();
+	    	$http.post(url, tweet_query).success(function(tweet_list){
+	    		if(tweet_list.tweets.length == 0 && tweet_list.number_found == 0){
+	    			var error = {type: 0, message: "Tweets no found", data: tweet_list};
+	    			dfd.reject(error);
+	    		}else{
+	    			dfd.resolve(tweet_list);
+	    		}
+	    	}).error(function(err){
+	    			var error = {type: 1, message: JSON.stringify(err), data: err};
+	    			dfd.reject(error);
+	    	});
+	    	return dfd.promise;
+	    }
+
 		return {
 			
-			list: function(){
-				get_analysis(data);
-				return ['a', 'b','c'];
+			get_tweets: function(tweet_query){
+				if(tweet_query == null){
+					// Test purpose
+					tweet_query = tweet_query_dev;
+				}
+				return get_tweets(tweet_query);
+			},
+			get_analysis: function (sentiment_query) {
+				if(sentiment_query == null){
+					// Test purpose
+					sentiment_query = sentiment_query_dev;
+				}
+				return get_analysis(sentiment_query);
 			}
 		}
 	}
